880d3dabd93d4f817390a9cc307d2ff6181ea963
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-24 17:33:39 +0500

    add hello route

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"example.com/m/v2/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnv()
+
+}
+
+func main() {
+	route := gin.Default()
+
+	route.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+	
+	route.Run() // listen and serve on 0.0.0.0:8080
+}
